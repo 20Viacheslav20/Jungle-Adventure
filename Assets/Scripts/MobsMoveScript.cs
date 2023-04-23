caccf060e1823335a7ae883277ddbcd386f65151
Viacheslav <viacheslavdmytrenko03@gmail.com>
2023-04-23 18:19:52 +0200

    add enemies, add death

---
+++ Assets/Scripts/MobsMoveScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobsMoveScript : MonoBehaviour
+{
+    private Transform currentPoint;
+    private Rigidbody2D rigidbody2d;
+
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    [SerializeField] private bool isMoveVertical;
+
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentPoint = pointA.transform;
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        if (isMoveVertical)
+        {
+            if (currentPoint == pointA.transform)
+            {
+                rigidbody2d.velocity = new Vector2(0, -speed);
+            }
+            else
+            {
+                rigidbody2d.velocity = new Vector2(0, speed);
+            }
+        } else
+        {
+            if (currentPoint == pointA.transform)
+            {
+                rigidbody2d.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                rigidbody2d.velocity = new Vector2(-speed, 0);
+            }
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            Flip();
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            Flip();
+            currentPoint = pointB.transform;
+        }
+        
+    }
+
+    private void Flip()
+    {
+        if(!isMoveVertical) 
+        { 
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Player/PlayerControllerScript.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private BoxCollider2D boxCollider2d;
+    private ItemCollectorScript collector;
+    private Vector2 startPosition;
+
+    private float dirX = 0f;
+
+    [SerializeField] private float speed = 6f;
+    [SerializeField] private float jumpForce = 6f;
+    [SerializeField] private LayerMask layerMask;
+    
+    private enum MovementState 
+    { 
+        Idle = 0, 
+        Running = 1, 
+        Jumping = 2
+    }
+
+    private MovementState state = MovementState.Idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        collector = GetComponent<ItemCollectorScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (rigidbody2d.bodyType != RigidbodyType2D.Static)
+        {
+            dirX = Input.GetAxis("Horizontal");
+            rigidbody2d.velocity = new Vector2(dirX * speed, rigidbody2d.velocity.y);
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) && IsGrounded()) 
+            {
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
+            }
+            UpdateAnimationState();
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            Die();
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            RestartLevel();
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (collision.contacts[0].normal == Vector2.up)
+            {
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, 4f);
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                IncrementLives();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("KillZone"))
+        {
+            IncrementLives();
+        }
+    }
+
+    private void Die()
+    {
+        animator.SetTrigger("death");
+        rigidbody2d.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void UpdateAnimationState()
+    {
+
+        if (dirX > 0f)
+        {
+            spriteRenderer.flipX = false;
+            state = MovementState.Running;
+        }
+        else if (dirX < 0f)
+        {
+            spriteRenderer.flipX = true;
+            state = MovementState.Running;
+        }
+        else
+        {
+            state = MovementState.Idle;
+        }
+
+        if (!IsGrounded() /*&& (rigidbody2d.velocity.y > .1f || rigidbody2d.velocity.y <= -.1f)*/)
+        {
+            state = MovementState.Jumping;
+        }
+        animator.SetInteger("state", (int)state);
+    }
+
+    private void Respawn()
+    {
+        StartCoroutine(Blink());
+        transform.position = startPosition;    
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
+        collector.CountOfLives = 4;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, layerMask);
+    }
+
+    private IEnumerator Blink()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Color originalColor = spriteRenderer.color;
+
+        for (int i = 0; i < 3; i++)
+        {
+            spriteRenderer.color = Color.red;
+            yield return new WaitForSeconds(0.1f);
+            spriteRenderer.color = originalColor;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void IncrementLives()
+    {
+        collector.CountOfLives--;
+        if (collector.CountOfLives != 0)
+            Respawn();
+        else
+            Die();
+    }
+
+}
